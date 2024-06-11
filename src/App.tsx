c5c027c4768a49e7572748e70a5800928b301a77
Saroj Tamang <sarojtmg992@gmail.com>
2024-06-11 19:15:02 +0100

    header creating

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <header>
+
+      <div>
+        <img src="" alt="" />
+        <button>Sign in</button>
+      </div>
+     </header>
+    </>
+  )
+}
+
+export default App
