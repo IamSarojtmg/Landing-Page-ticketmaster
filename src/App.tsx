2d0c751ba7d6991840d079890babcb55f1bef651
Saroj Tamang <sarojtmg992@gmail.com>
2024-06-12 14:55:58 +0100

    heeader items

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-
 import './App.css'
-
+import background from './assets/livefest.webp'
 function App() {
 
 
   return (
     <>
-     <header>
-
-      <div>
-        <img src="" alt="" />
+     <header className='main-header'>
+      <img className='background' src={background} alt="" />
+      <div className='top-header'>
+        <h2><em>ticketmaster</em></h2>
         <button>Sign in</button>
       </div>
+      <section>
+        <h1>Rock Your Sales on Ticketmaster!</h1>
+        <p>Connect with buyers and sell faster.</p>
+        <p>Ready to sell? Enter your email to create and start selling</p>
+        <input type="text" />
+        <button> Get Started</button>
+      </section>
      </header>
     </>
   )
