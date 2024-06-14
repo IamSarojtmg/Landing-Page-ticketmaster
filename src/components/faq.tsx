f3e53bcd993d5b1d5551cb91b40400da0ed46f2e
Saroj Tamang <sarojtmg992@gmail.com>
2024-06-14 16:17:48 +0100

    animation

--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -1,27 +1,56 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export const Faq = ()=>{
+export const Faq = () => {
+  const [count, setCount] = useState<number>(2);
+  const [hidden, setHide] = useState<boolean>(false);
+  const [animate,setAnimate]= useState<boolean>()
 
-    const [hide, setHide]=useState<Boolean>(true)
+  function showFaq() {
+   let newCount = 1
+    
+    setCount((e) => {
+      newCount = e + 1;     
+      return newCount;
+    });
 
-    //use clicks on the h3 > setHide to false
-    //if hide = false than show the p
-    //how to make sure that it only does that with one of the faq and not open to whole faqs. Maybe for that try a new classname for each questions
+    console.log(count%2===0);
+    if(count%2===0){
+      setHide(true)
+    }else{
+      setHide(false)
+    }
 
-    return(
-        <>
-        <h1>FAQ</h1>
-        <section>
-            <div>
-            <h3>How do I start selling on Ticketmaster</h3>
-            <p>To start selling, create a seller account by entering your email and following the setup instructions.</p>
-            </div>
-            <div>
-            <h3>What types of tickets can I sell?</h3>
-            <p>You can sell tickets for concerts, sports events, theater shows, and more..</p>
-            </div>
+    if(hidden){
 
-        </section>
-        </>
-    )
-}
+    }
+  }
+
+  const ansOne = <div className="ans">To start selling, create a seller account by entering your email and following the setup instructions.</div>
+  const  close = <span></span>
+
+
+
+
+  return (
+    <main className="faq-comp">
+      <h1>FAQ</h1>
+
+        <div className="qna">
+          <h3 className="questions" onClick={() => showFaq()}>
+            How do I start selling on Ticketmaster?
+          </h3>
+          {hidden?ansOne:close}
+        </div>
+
+        <div>
+          <h3>What types of tickets can I sell?</h3>
+          <p>
+            You can sell tickets for concerts, sports events, theater shows, and
+            more..
+          </p>
+        </div>
+
+    </main>
+
+  );
+};
