4f27bd8b67bfb898b1ebdda37ec6c268e7c06123
Saroj Tamang <sarojtmg992@gmail.com>
2024-06-15 10:56:23 +0100

    animation

--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -3,54 +3,55 @@ import { useState } from "react";
 export const Faq = () => {
   const [count, setCount] = useState<number>(2);
   const [hidden, setHide] = useState<boolean>(false);
-  const [animate,setAnimate]= useState<boolean>()
+  const [animate, setAnimate] = useState<boolean>(false);
 
   function showFaq() {
-   let newCount = 1
-    
+    let newCount = 1;
+
     setCount((e) => {
-      newCount = e + 1;     
+      newCount = e + 1;
       return newCount;
     });
 
-    console.log(count%2===0);
-    if(count%2===0){
-      setHide(true)
-    }else{
-      setHide(false)
-    }
-
-    if(hidden){
-
+    if (count % 2 === 0) {
+      setHide(true);
+    } else {
+      setHide(false);
     }
   }
 
-  const ansOne = <div className="ans">To start selling, create a seller account by entering your email and following the setup instructions.</div>
-  const  close = <span></span>
+  const ansOne = (
+    <div className="ans">
+      To start selling, create a seller account by entering your email and
+      following the setup instructions.
+    </div>
+  );
+  const close = <span></span>;
 
-
-
+  //changes on line 41 - setted the class toggel according to the hidden use state
+  //go to css file
 
   return (
     <main className="faq-comp">
       <h1>FAQ</h1>
 
-        <div className="qna">
-          <h3 className="questions" onClick={() => showFaq()}>
-            How do I start selling on Ticketmaster?
-          </h3>
-          {hidden?ansOne:close}
-        </div>
+      <div className="qna">
+        <h3 className="questions" onClick={() => showFaq()}>
+          How do I start selling on Ticketmaster?
+        </h3>
+        <div className={hidden?'ans':'hide'}>
+      To start selling, create a seller account by entering your email and
+      following the setup instructions.
+    </div>
+      </div>
 
-        <div>
-          <h3>What types of tickets can I sell?</h3>
-          <p>
-            You can sell tickets for concerts, sports events, theater shows, and
-            more..
-          </p>
-        </div>
-
+      <div>
+        <h3>What types of tickets can I sell?</h3>
+        <p>
+          You can sell tickets for concerts, sports events, theater shows, and
+          more..
+        </p>
+      </div>
     </main>
-
   );
 };
