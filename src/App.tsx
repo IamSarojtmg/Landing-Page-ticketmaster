3f4952cc22bc5148a00d7c18e568d21a6c8da8ac
Saroj Tamang <sarojtmg992@gmail.com>
2024-06-17 18:37:04 +0100

    stating css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Faq } from "./components/faq";
+import { Faq } from "./components/x";
 import {Header} from "./components/header"
 import { Steps } from "./components/steps";
 function App() {
@@ -13,3 +13,8 @@ function App() {
 }
 
 export default App;
+
+//empty object - it will have a key property with a number value and will retirn a boolean?
+
+//turining in shallow copy so it wont mutate
+// 1: false
--- src/components/x.tsx
+++ src/components/x.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 
 export const Faq = () => {
+  const [visibleAnswers, setVisibleAnswers] = useState([false, false]);
   const [count, setCount] = useState<number>(2);
   const [hidden, setHide] = useState<boolean>(false);
 
-  function showFaq() {
+  const toggleAnswer = (index: number) => {
     let newCount = 1;
 
     setCount((e) => {
@@ -18,23 +19,20 @@ export const Faq = () => {
     } else {
       setHide(false);
     }
-  }
+    setVisibleAnswers((prevVisibleAnswers) => {
+      const newVisibleAnswers = [...prevVisibleAnswers];
+      newVisibleAnswers[index] = !newVisibleAnswers[index];
+      return newVisibleAnswers;
+    });
+  };
 
-  interface qandaInterface {
-    id: number
-    question: string;
-    answer: string;
-  }
-
-  const questionAnswers: qandaInterface[] = [
+  const questionAnswers = [
     {
-      id:1,
       question: "How do I start selling on Ticketmaster?",
       answer:
         "To start selling, create a seller account by entering your email and following the setup instructions.",
     },
     {
-      id:2,
       question: "What types of tickets can I sell?",
       answer:
         "You can sell tickets for concerts, sports events, theater shows, and more.",
@@ -45,51 +43,19 @@ export const Faq = () => {
     <main className="faq-comp">
       <h1>FAQ</h1>
       <div className="qna">
-
-      {questionAnswers.map((e, index) => (
-        
-        <div key={index}>         
-          <h3 className="questions" onClick={showFaq}>
-            {e.question}
-          </h3>
-          <div className={`answer ${hidden ? "show" : "hide"}`}>{e.answer}</div>
-        </div>
-      ))}
-
-
-
-
-        {/* <h3 className="questions" onClick={() => showFaq()}>
-          How do I start selling on Ticketmaster?
-        </h3>
-
-        <div className={`answer ${hidden ? "show" : "hide"}`}>
-          To start selling, create a seller account by entering your email and
-          following the setup instructions.
-        </div> */}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        {questionAnswers.map((qa, index) => (
+          <div key={index}>
+            <h3 className="questions" onClick={() => toggleAnswer(index)}>
+              {qa.question}
+            </h3>
+            {visibleAnswers[index] && (
+              <div className={`answer${hidden ? "show" : "hide"}`}>
+                {qa.answer}
+              </div>
+            )}
+          </div>
+        ))}
       </div>
-
-
-
-
-
     </main>
   );
 };
